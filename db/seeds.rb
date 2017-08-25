137992662e60232622ed3413eccd5eb95a8d8b23
Silver Vanus <silvervanus@Silvers-MacBook-Pro.local>
2017-08-25 14:02:00 -0600

    created seeds file for sample data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,29 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+10.times do |blog|
+  Blog.create!(
+  title: "My Blog Post: #{blog}",
+  body: "None of this matter what is in here but for some reason it is breaking my code and it is annoying. Not sure why it is breaking it but apparantly such is the case."
+  )
+end
+
+puts "10 blog posts created"
+
+5.times do |skill|
+  Skill.create!(
+  title: "Rails #{skill}",
+  percent_utilized: 15
+  )
+end
+
+puts "5 skills created"
+
+9.times do |portfolio_item|
+  Portfolio.create!(
+    title: "Portfolio title: #{portfolio_item}",
+    subtitle: "My Great Service",
+    body: "Non of this matters what is in here. It is only a placeholder anyway and doesnt have anything to do with anything except practice my typing which needs alot of practice.",
+    main_image: "http://via.placeholder.com/350x150",
+    thumb_image: "http://via.placeholder.com/350x200",
+    )
+  end
+
+puts "9 portfolio items created"
