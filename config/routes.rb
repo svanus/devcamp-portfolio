33593d851ce95eabd6103671644e636472e380bd
Silver Vanus <silvervanus@Silvers-MacBook-Pro.local>
2017-08-31 08:26:12 -0600

    Customized portfolio routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :portfolios
+  resources :portfolios, except: [:show]
+
+get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
 
 #pages is contoller / about is action
   get 'about-me', to: 'pages#about'
