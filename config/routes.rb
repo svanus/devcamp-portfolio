833074c2254d6b3bffc006ec8bff737e6e4fad48
Silver <silvervanus@gmail.com>
2017-09-11 16:28:15 -0600

    Implemented custom scopes for portfolio items

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   resources :portfolios, except: [:show]
+  get 'angular_items', to: 'portfolios#angular'
 
-get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
+  get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
 
 #pages is contoller / about is action
   get 'about-me', to: 'pages#about'
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
+3.times do |topic|
+  Topic.create!(
+    title: "Topic #{topic}"
+    )
+end
+
+puts "3 Topics created"
+
+
 10.times do |blog|
   Blog.create!(
   title: "My Blog Post: #{blog}",
-  body: "None of this matter what is in here but for some reason it is breaking my code and it is annoying. Not sure why it is breaking it but apparantly such is the case."
+  body: "None of this matter what is in here but for some reason it is breaking my code and it is annoying. Not sure why it is breaking it but apparantly such is the case.",
+  topic_id: Topic.last.id
   )
 end
 
@@ -16,10 +26,20 @@ end
 
 puts "5 skills created"
 
-9.times do |portfolio_item|
+8.times do |portfolio_item|
   Portfolio.create!(
     title: "Portfolio title: #{portfolio_item}",
-    subtitle: "My Great Service",
+    subtitle: "Ruby on Rails",
+    body: "Non of this matters what is in here. It is only a placeholder anyway and doesnt have anything to do with anything except practice my typing which needs alot of practice.",
+    main_image: "http://via.placeholder.com/350x150",
+    thumb_image: "http://via.placeholder.com/350x200",
+    )
+  end
+
+  1.times do |portfolio_item|
+  Portfolio.create!(
+    title: "Portfolio title: #{portfolio_item}",
+    subtitle: "Angular",
     body: "Non of this matters what is in here. It is only a placeholder anyway and doesnt have anything to do with anything except practice my typing which needs alot of practice.",
     main_image: "http://via.placeholder.com/350x150",
     thumb_image: "http://via.placeholder.com/350x200",
